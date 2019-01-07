9b4af079684a93ad7f5af684f0237eb8df418a5e
Aszek18 <pawelczykaga@gmail.com>
2019-01-07 20:34:02 +0100

    ddee

--- web_apps/biblioteka/books/models.py
+++ web_apps/biblioteka/books/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Author(models.Model):
+    imie =  models.CharField(max_length=200)
+    nazwisko =  models.CharField(max_length=200)
 class Books (models.Model):
     title = models.CharField(max_length=200)
-    author = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete = models.CASCADE()
     description = models.TextField(max_length=200)
     cover = models.ImageField(upload_to="books_covers/%Y/%m/&d")
 
--- web_apps/biblioteka/books/views.py
+++ web_apps/biblioteka/books/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+import...
+from books.views
+def book_list(request):
+    books = Book.objects.all()
+    return
